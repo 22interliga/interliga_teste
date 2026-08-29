fbe5d193e07ccb81d22d505b2426a1507205644a
22interliga <22mobi2025@gmail.com>
2026-08-29 15:54:37 -0300

    Corrige configuracao Firebase homologacao conforme SDK oficial

--- firebase-interfood-config-teste.js
+++ firebase-interfood-config-teste.js
@@ -1,12 +1,12 @@
 // AMBIENTE DE HOMOLOGACAO INTERLIGA / INTERFOOD
 // Projeto Firebase separado da producao.
 window.INTERFOOD_FIREBASE_TEST_CONFIG = {
-  apiKey: "AIzaSyBWMmsff1hV-6vDuQp3EK-EvSkkhYVBRFE",
+  apiKey: "AIzaSyBwMMsff1hV-6vDuQb3EK-EvSkkhYVBRFE",
   authDomain: "interliga-homologacao-eb0f2.firebaseapp.com",
   projectId: "interliga-homologacao-eb0f2",
   storageBucket: "interliga-homologacao-eb0f2.firebasestorage.app",
   messagingSenderId: "997118774501",
-  appId: "1:997118774501:web:59f5bea39ed070986d180c"
+  appId: "1:997118774501:web:59f56ea39ed070986d180c"
 };
 
 // Trava: nunca aceitar o projeto de producao nesta camada de homologacao.
