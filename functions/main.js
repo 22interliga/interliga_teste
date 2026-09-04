3df3c8ecf526584cac64fb3865e8be64c368d5de
22interliga <22mobi2025@gmail.com>
2026-09-04 10:03:15 -0300

    feat: exportar backend de fechamentos Interfood

--- functions/main.js
+++ functions/main.js
@@ -1,3 +1,4 @@
 const base = require('./index');
 const seguro = require('./pedido-coordenadas');
-module.exports = Object.assign({}, base, seguro);
+const fechamentos = require('./fechamentos-interfood');
+module.exports = Object.assign({}, base, seguro, fechamentos);
