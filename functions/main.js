c659b99b17cb8c16e4b2851d3a2f1a3101ccc5c9
22interliga <22mobi2025@gmail.com>
2026-09-05 19:58:20 -0300

    Homologação: exportar funções de push Interfood

--- functions/main.js
+++ functions/main.js
@@ -1,4 +1,5 @@
 const base = require('./index');
 const seguro = require('./pedido-coordenadas');
 const fechamentos = require('./fechamentos-interfood');
-module.exports = Object.assign({}, base, seguro, fechamentos);
+const push = require('./push-interfood');
+module.exports = Object.assign({}, base, seguro, fechamentos, push);
