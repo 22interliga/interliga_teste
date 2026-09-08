f200bd552301d914f7e2dbbf9a1f54e27b399735
22interliga <22mobi2025@gmail.com>
2026-09-08 17:19:39 -0300

    Usa wrapper App Check na análise de cardápio

--- functions/main.js
+++ functions/main.js
@@ -2,4 +2,5 @@ const base = require('./index');
 const seguro = require('./pedido-coordenadas');
 const fechamentos = require('./fechamentos-interfood');
 const push = require('./push-interfood');
-module.exports = Object.assign({}, base, seguro, fechamentos, push);
+const cardapioImagem = require('./cardapio-imagem-appcheck');
+module.exports = Object.assign({}, base, seguro, fechamentos, push, cardapioImagem);
